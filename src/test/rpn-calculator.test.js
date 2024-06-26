7c2aea360f8d37d50abfbb2972c8a308c8fa6d78
johannes60-sk <garilsenan@gmail.com>
2024-06-26 16:03:00 +0200

    Add test for MAX operation followed by addition  in RPNCalculator()

--- src/test/rpn-calculator.test.js
+++ src/test/rpn-calculator.test.js
@@ -20,3 +20,7 @@ test("4 2 + 3 - should return 3", () => {
   test("5 3 4 2 9 1 MAX should return 9", () => {
     expect(RPNCalculator("5 3 4 2 9 1 MAX")).toBe(9);
   });
+
+  test("4 5 MAX 1 + should return 6", () => {
+    expect(RPNCalculator("4 5 MAX 1 +")).toBe(6);
+  });
