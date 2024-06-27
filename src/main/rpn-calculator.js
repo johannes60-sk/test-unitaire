f9ed1cdff519baabe982abbfa6a9d632d2df47e0
johannes60-sk <garilsenan@gmail.com>
2024-06-27 11:38:08 +0200

    run  test-coverage

--- src/main/rpn-calculator.js
+++ src/main/rpn-calculator.js
@@ -1,44 +1,45 @@
 export const RPNCalculator = (expression) => {
-  const elements = expression.split(" ");
-  const stack = [];
-
-  elements.forEach((element) => {
-    if (!isNaN(element)) {
-      stack.push(parseFloat(element));
-    } else if (["+", "-", "*", "/"].includes(element)) {
-      const num2 = stack.pop();
-      const num1 = stack.pop();
-      let result;
-      switch (element) {
-        case "+":
-          result = num1 + num2;
-          break;
-        case "-":
-          result = num1 - num2;
-          break;
-        case "*":
-          result = num1 * num2;
-          break;
-        case "/":
-          result = num1 / num2;
-          break;
+    const elements = expression.split(" ");
+    const stack = [];
+  
+    elements.forEach((element) => {
+      if (!isNaN(element)) {
+        stack.push(parseFloat(element));
+      } else if (["+", "-", "*", "/"].includes(element)) {
+        const num2 = stack.pop();
+        const num1 = stack.pop();
+        let result;
+        switch (element) {
+          case "+":
+            result = num1 + num2;
+            break;
+          case "-":
+            result = num1 - num2;
+            break;
+          case "*":
+            result = num1 * num2;
+            break;
+          case "/":
+            result = num1 / num2;
+            break;
+        }
+        stack.push(result);
+      } else if (element === "SQRT") {
+        const num = stack.pop();
+        stack.push(Math.sqrt(num));
+      } else if (element === "MAX") {
+        const maxVal = Math.max(...stack);
+        stack.length = 0; 
+        stack.push(maxVal);
+      } else {
+        throw new Error(`Invalid token: ${element}`);
       }
-      stack.push(result);
-    } else if (element === "SQRT") {
-      const num = stack.pop();
-      stack.push(Math.sqrt(num));
-    } else if (element === "MAX") {
-      const maxVal = Math.max(...stack);
-      stack.length = 0; 
-      stack.push(maxVal);
-    } else {
-      throw new Error(`Invalid token: ${element}`);
+    });
+  
+    if (stack.length !== 1) {
+      throw new Error("Invalid RPN expression");
     }
-  });
-
-  if (stack.length !== 1) {
-    throw new Error("Invalid RPN expression");
-  }
-
-  return stack[0];
-};
+  
+    return stack[0];
+  };
+  
